55cf103da35423c1f129ac3b083b0a06c1d8e556
NASA-0007 <116829149+NASA-0007@users.noreply.github.com>
2022-11-17 06:01:19 +0530

    Update TABLE.py

--- TABLE.py
+++ TABLE.py
@@ -1,6 +1,7 @@
-import mysql.connector as sql
-conn=sql.connect(host='localhost',user='root',passwd='glhf123',database='bank')
+import  mysql.connector as sql
+conn=sql.connect(host='localhost',user='root',passwd='glhf123', database='bank')
 if conn.is_connected():
     print('connected succesfully')
-    cur = conn.cursor()
-    cur.execute('create table customer_details(acct_no int primary key,acct_namevarchar(25) ,phone_nobigint(25) check(phone_no>11),address varchar(25),cr_amt float )')
+cur = conn.cursor()
+cur.execute('drop table user_table;')
+cur.execute('create table customer_det(acct_no int primary key,acct_name varchar(25),phone_no bigint(25),address varchar(25),cr_amt float);')
