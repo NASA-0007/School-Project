9a895a8fecaf6a87958a4e681ee9cf5a122d2051
NASA-0007 <116829149+NASA-0007@users.noreply.github.com>
2022-11-17 06:01:43 +0530

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,21 +1,23 @@
 import mysql.connector as sql
+import TABLE
 conn=sql.connect(host='localhost',user='root',passwd='glhf123',database='bank')
 cur = conn.cursor()
-cur.execute('create table user_table(acct_no int ,acct_name varchar(25) ,phone_nobig int(25) check(phone_no>11),address varchar(25),cr_amt float )')
+cur.execute('create table user_table(username varchar(25) primarykey,passwrd varchar(25) not null )')
 print('1.REGISTER')
 print('2.LOGIN')
 n=int(input('enter your choice='))
 if n== 1:
     name=input('Enter a Username=')
     passwd=int(input('Enter a 4 DIGIT Password='))
-    V_SQLInsert="INSERT  INTO user_table (passwrd,username) values ("+str(passwd)+",'"+name+"')"
+    V_SQLInsert=("INSERT INTO user_table values("+str(passwd)+","+name+") ")
     cur.execute(V_SQLInsert)
     conn.commit()
+    TABLE
     print('USER created succesfully')
 if  n==2 :
     name=input('Enter your Username=')
     passwd=int(input('Enter your 4 DIGIT Password='))
-    V_Sql_Sel="select * from user_table where passwrd='"+str (passwd)+"' and username=  ' " +name+ " ' "
+    V_Sql_Sel=("select * from user_table where passwd='"+str (passwd)+"'and username='"+name+"'")
     cur.execute(V_Sql_Sel)
 if cur.fetchone() is  None:
     print('Invalid username or password')
