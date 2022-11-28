74000ae0234bb48a22534c9e6a99147ba9314b5c
NASA-0007 <116829149+NASA-0007@users.noreply.github.com>
2022-11-28 21:59:05 +0530

    Delete TABLE.py

--- TABLE.py
+++
@@ -1,7 +0,0 @@
-import  mysql.connector as sql
-conn=sql.connect(host='localhost',user='root',passwd='glhf123', database='bank')
-if conn.is_connected():
-    print('connected succesfully')
-cur = conn.cursor()
-cur.execute('drop table user_table;')
-cur.execute('create table customer_det(acct_no int primary key,acct_name varchar(25),phone_no bigint(25),address varchar(25),cr_amt float);')
