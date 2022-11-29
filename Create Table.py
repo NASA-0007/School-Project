b24eda5e709835fe39e29d3e384b063e46a76412
NASA-0007 <116829149+NASA-0007@users.noreply.github.com>
2022-11-29 18:42:03 +0530

    Create Create Table.py

---
+++ Create Table.py
@@ -0,0 +1,7 @@
+import  mysql.connector as sql
+conn=sql.connect(host='localhost',user='root',passwd='glhf123', database='bank')
+if  conn.is_connected():
+    print('connected succesfully')
+cur = conn.cursor()
+cur.execute('create table customer_details(acct_no int primary key,acct_name varchar(25),phone_no bigint(25),address varchar(25),cr_amt float)')
+cur.execute('create table user_table(username varchar(25) primary key,passwrd varchar(25) not null)')
